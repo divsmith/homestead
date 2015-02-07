d243adebab664b94d356c1531a71a4b69baaff45
Parker Smith <parker@parkersmith.us>
2015-02-07 02:06:30 -0700

    Remove git configuration

--- scripts/after.sh
+++ scripts/after.sh
@@ -7,16 +7,5 @@
 # Update just for good measure
 sudo apt-get update
 
-# Git Configuration
-name="Parker Smith"
-email="parker@parkersmith.us"
-
-config="
-[user]
-	name=$name
-	email=$email"
-
-echo "$config" > /home/vagrant/.gitconfig
-
 # Update npm
 sudo npm update npm -g
